caf7ebc2b91d37b531341c3f18c5a45f15ae4709
GRayHook <s@marinkevich.ru>
2017-11-01 21:47:59 +0700

    Img's paths (1)

--- app/views/projects_photoslider.php
+++ app/views/projects_photoslider.php
@@ -3,9 +3,9 @@
 
 <div id = "slider1"></div>
 <div id = "slider2">
-  <img src = "img/js-photo-slider/1.jpg" height = "300" width = "450">
-  <img src = "img/js-photo-slider/2.jpg" height = "100" width = "150">
+  <img src = "/img/js-photo-slider/1.jpg" height = "300" width = "450">
+  <img src = "/img/js-photo-slider/2.jpg" height = "100" width = "150">
   <div class = "drisnya" height = "100" width = "150">Что-нибудь</div>
-  <img src = "img/js-photo-slider/3.jpg" height = "1" width = "1">
+  <img src = "/img/js-photo-slider/3.jpg" height = "1" width = "1">
 </div>
 <div id = "slider3"></div>
